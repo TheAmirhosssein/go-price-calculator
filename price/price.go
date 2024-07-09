fc02badd328f87242d6a554aef3e4cc1d38c3e1d
amirhossein <amirhosseinmobini9@gmail.com>
2024-07-09 21:39:36 +0330

    ✨feature: constructor for tax include struct added

--- price/price.go
+++ price/price.go
@@ -5,3 +5,11 @@ type TaxIncludeJobPrice struct {
 	InputPrices      []float64
 	TaxIncludedPrice map[string]float64
 }
+
+func NewTaxIncludeJobPrice(taxRate float64) *TaxIncludeJobPrice {
+	return &TaxIncludeJobPrice{
+		TaxRate:          taxRate,
+		InputPrices:      []float64{20, 10, 30},
+		TaxIncludedPrice: make(map[string]float64),
+	}
+}
