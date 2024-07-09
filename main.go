57199b81fc41fa4733b2f5949869f4450c3eeb36
amirhossein <amirhosseinmobini9@gmail.com>
2024-07-09 23:07:56 +0330

    ✨feature: process method added

--- main.go
+++ main.go
@@ -1,19 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/TheAmirhosssein/go-price-calculator/price"
+)
 
 func main() {
-	prices := []float64{20, 30}
 	taxRates := []float64{0, 0.07, 0.1, 0.2}
-	taxRatesPrices := make(map[float64][]float64)
 
 	for _, taxRate := range taxRates {
-		calculatedTaxRate := make([]float64, len(prices))
-		for priceIndex, price := range prices {
-			calculatedTaxRate[priceIndex] = price * (1 + taxRate)
-		}
-		taxRatesPrices[taxRate] = calculatedTaxRate
+		jobTax := price.NewTaxIncludeJobPrice(taxRate)
+		jobTax.Process()
+		fmt.Println(jobTax.TaxIncludedPrice)
 	}
-
-	fmt.Println(taxRatesPrices)
 }
--- price/price.go
+++ price/price.go
@@ -1,15 +1,23 @@
 package price
 
+import "fmt"
+
 type TaxIncludeJobPrice struct {
 	TaxRate          float64
 	InputPrices      []float64
 	TaxIncludedPrice map[string]float64
 }
 
-func NewTaxIncludeJobPrice(taxRate float64) *TaxIncludeJobPrice {
-	return &TaxIncludeJobPrice{
+func (job *TaxIncludeJobPrice) Process() {
+	for _, price := range job.InputPrices {
+		job.TaxIncludedPrice[fmt.Sprintf("%.2f", price)] = price * (1 + job.TaxRate)
+	}
+}
+
+func NewTaxIncludeJobPrice(taxRate float64) TaxIncludeJobPrice {
+	return TaxIncludeJobPrice{
 		TaxRate:          taxRate,
-		InputPrices:      []float64{20, 10, 30},
+		InputPrices:      []float64{20, 10},
 		TaxIncludedPrice: make(map[string]float64),
 	}
 }
