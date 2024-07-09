69d3d47bdd6e5917834b4d24ed0d166b60e1b18b
amirhossein <amirhosseinmobini9@gmail.com>
2024-07-09 19:57:54 +0330

    ✨feature: base logic implemented

--- main.go
+++ main.go
@@ -1,3 +1,19 @@
 package main
 
-func main() {}
+import "fmt"
+
+func main() {
+	prices := []float64{20, 30}
+	taxRates := []float64{0, 0.07, 0.1, 0.2}
+	taxRatesPrices := make(map[float64][]float64)
+
+	for _, taxRate := range taxRates {
+		calculatedTaxRate := make([]float64, len(prices))
+		for priceIndex, price := range prices {
+			calculatedTaxRate[priceIndex] = price * (1 + taxRate)
+		}
+		taxRatesPrices[taxRate] = calculatedTaxRate
+	}
+
+	fmt.Println(taxRatesPrices)
+}
