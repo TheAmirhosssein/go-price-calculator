4da2230f07020fc8bb5e2be56e9c7d6c9f818f6b
amirhossein <amirhosseinmobini9@gmail.com>
2024-07-13 23:57:58 +0330

    ✨feature: file manager and result json added into project

--- main.go
+++ main.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"fmt"
+
+	"github.com/TheAmirhosssein/go-price-calculator/cmdmanager"
 	"github.com/TheAmirhosssein/go-price-calculator/filemanger"
+	"github.com/TheAmirhosssein/go-price-calculator/iomanager"
 	"github.com/TheAmirhosssein/go-price-calculator/price"
 )
 
@@ -9,8 +13,19 @@ func main() {
 	taxRates := []float64{0, 5, 6}
 
 	for _, taxRate := range taxRates {
-		fm := filemanger.New("price.txt", "result.json")
-		jobTax := price.NewTaxIncludedPriceJob(fm, taxRate)
+		var command string
+		fmt.Print("enter 'file' for file entry and 'cmd' for cmd: ")
+		fmt.Scan(&command)
+		var io iomanager.IOManager
+		if command == "cmd" {
+			io = cmdmanager.New()
+		} else if command == "file" {
+			io = filemanger.New("price.txt", "result.json")
+		} else {
+			fmt.Println("are you fucking retarded?")
+			return
+		}
+		jobTax := price.NewTaxIncludedPriceJob(io, taxRate)
 		jobTax.Process()
 	}
 }
